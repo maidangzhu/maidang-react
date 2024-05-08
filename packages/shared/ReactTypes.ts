a34a5bf19e1e76d9802bf7248a7b6b16e679dd9b
朱建业 <borisdunk@sina.com>
2024-05-08 20:38:21 +0800

    feat: jsx runtime export and add rollup config file

--- packages/shared/ReactTypes.ts
+++ packages/shared/ReactTypes.ts
@@ -1,14 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Type = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Key = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Props = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Ref = any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ElementType = any;
 
 export interface ReactElementType {
-	$$typeof: symbol | number;
-	key: Key;
-	props: Props;
-	ref: Ref;
-	type: ElementType;
-	__mark: string;
+  $$typeof: symbol | number;
+  key: Key;
+  props: Props;
+  ref: Ref;
+  type: ElementType;
+  __mark: string;
 }
